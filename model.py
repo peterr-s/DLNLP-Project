0bbbfb96bf0abcdd3eb4d5537697e716b982752b
Peter Schoener <peter.c.schoener@gmail.com>
2017-09-12 04:21:13 -0700

    tried making the dtype param of map_fn explicit; no effect

--- model.py
+++ model.py
@@ -31,15 +31,17 @@ class Model:
 		# convert to embeddings
 		self._embedding_model = embedding_model
 		self._numberer = numberer
-		input_layer = tf.map_fn(self.sent_embed, self._x)
+		input_layer = tf.map_fn(self.sent_embed, self._x, dtype = tf.int32)
 		# input_layer = self._x
 
 		# make a bunch of LSTM cells and link them
 		# use rnn.DropoutWrapper instead of tf.nn.dropout because the layers are anonymous
 		stacked_LSTM = rnn.MultiRNNCell([rnn.DropoutWrapper(rnn.BasicLSTMCell(config.LSTM_sz), output_keep_prob = config.dropout_ratio) for _ in range(config.LSTM_ct)])
 		
+		#import pdb; pdb.set_trace()
+		
 		# run the whole thing
-		_, hidden = tf.nn.dynamic_rnn(stacked_LSTM, input_layer, sequence_length = self._lens, dtype = tf.float32)
+		_, hidden = tf.nn.dynamic_rnn(stacked_LSTM, input_layer, sequence_length = self._lens, dtype = tf.int32)
 		w = tf.get_variable("W", shape=[hidden[-1].h.shape[1], label_size]) # if I understood the structure of MultiRNNCell correctly, hidden[-1] should be the final state
 		b = tf.get_variable("b", shape=[1])
 		logits = tf.matmul(hidden[-1].h, w) + b
@@ -67,14 +69,15 @@ class Model:
 			self._accuracy = tf.reduce_mean(correct)
 
 	def sent_embed(self, sent_arr) :
-		return tf.map_fn(self.get_embedding, sent_arr)
+		return tf.map_fn(self.get_embedding, sent_arr, dtype = tf.int32)
 	
 	def get_embedding(self, word_int) :
 		word = self._numberer.value(word_int)
 		if word in self._embedding_model.wv :
-			return self._embedding_model.wv[word]
+			print(word, self._embedding_model.wv[word])
+			return (self._embedding_model.wv[word] * 100).astype(numpy.int32)
 		else :
-			return numpy.zeros(self._embedding_model.vector_size)
+			return numpy.zeros(self._embedding_model.vector_size).astype(numpy.int32)
 	
 	@property
 	def accuracy(self):
