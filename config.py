d8dc9e9cee9a5a81918891a85971fc2ce68e82c4
Peter Schoener <peter.c.schoener@gmail.com>
2017-09-11 17:07:29 -0700

    hyperparameter tuning and test input consolidation

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 class DefaultConfig:
-	n_epochs = 50
+	n_epochs = 10
 	batch_size = 512
 	#input_dropout = 0.95
 	#hidden_dropout = 0.95
-	max_timesteps = 20
-	LSTM_ct = 4
+	max_timesteps = 40
+	LSTM_ct = 10
 	LSTM_sz = 200
 	embedding_sz = 100
 	dropout_ratio = 0.9
